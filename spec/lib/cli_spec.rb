2cd782ade73fc7fe81732819d439b0fc507bbaa6
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2020-04-02 14:16:23 +0200

    Implement Cli class specs

---
+++ spec/lib/cli_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+shared_examples 'Cli test' do |description, args|
+  subject(:cli_object) { described_class.new(args) }
+
+  it description do
+    expect do
+      cli_object.call
+    end.to output("#{expectation}\n").to_stdout
+  end
+end
+
+describe Cli do
+  describe '#call' do
+    context 'when no arguments provided' do
+      let(:expectation) do
+        'Please provide a file location to get the webserver log report.'
+      end
+
+      include_examples 'Cli test', 'prompts about missing filepath', []
+    end
+
+    context 'when invalid filepath is provided' do
+      let(:expectation) do
+        'Could not open the file abc. Please verify the file location.'
+      end
+
+      before { allow(File).to receive(:open).and_raise(Errno::ENOENT) }
+
+      include_examples 'Cli test', 'prompts about invalid filepath', %w[abc]
+    end
+
+    context 'when valid filepath is provided' do
+      let(:file_content) { 'foo' }
+      let(:expectation) { file_content }
+      let(:file) { instance_double(File, read: file_content) }
+
+      before do
+        allow(File).to receive(:open).and_return(file)
+      end
+
+      include_examples 'Cli test', 'reads the file', ['filepath']
+    end
+  end
+end
+
