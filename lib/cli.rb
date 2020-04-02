c7f01a8926d71e0df1c188ffe959e84cb00ef913
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2020-04-02 14:44:51 +0200

    Implement Cli class

---
+++ lib/cli.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Entrypoint for arguments parsing and logic executing
+class Cli
+  def initialize(argv = [])
+    @argv = argv
+  end
+
+  def call
+    if argv.empty?
+      puts 'Please provide a file location to get the webserver log report.'
+    else
+      parse_webserver_log
+    end
+  rescue Errno::ENOENT
+    puts "Could not open the file #{argv[0]}. Please verify the file location."
+  end
+
+  private
+
+  attr_reader :argv
+
+  def file
+    File.open(argv[0], 'r')
+  end
+
+  def parse_webserver_log
+    WebserverLogParser.new(file).call
+  end
+end
