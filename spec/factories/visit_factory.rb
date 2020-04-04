88a591192ebd54efc4b84195c439a83de9f5764c
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2020-04-04 04:18:46 +0200

    Fix rubocop Style/SymbolProc warning

--- spec/factories/visit_factory.rb
+++ spec/factories/visit_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :visit do
     # Sequel model save! method is not defined
-    to_create { |i| i.save }
+    to_create(&:save)
 
     path { 'a' }
     visitor_identifier { '1' }
